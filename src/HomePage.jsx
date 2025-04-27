dacf8da54a237c6656de75ac1a6d6a5fb3763fb7
NMEJIA93 <norbeymejiacortes@gmail.com>
2025-04-26 22:20:46 -0500

    Install bootstrap icons

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -6,6 +6,7 @@ export const HomePage = () => {
             <h1 className="text-3xl font-bold underline text-blue-500">
                 Hello world!
             </h1>
+            <i className="bi bi-0-circle"></i>
         </div>
     )
 }
