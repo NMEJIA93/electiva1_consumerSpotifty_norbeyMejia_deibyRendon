a57239febe0b2dacfd8171de80e0c208ac2ed338
Deiby Rendon <deibyrh@iris.com.co>
2025-05-02 19:24:05 -0500

    feature/registerLogin
    Implementation of a background image in user registration

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { RegisterForm } from '../components/RegisterForm';
 import { FormActions } from '../components/FormActions';
+import Bacground from '../../assets/backgroundGreen.png'
 
 export const RegisterPage = () => {
-    const { handleCancel } = FormActions();
-
     return (
-        <div className="relative overflow-hidden min-h-screen bg-green-700">
+        <div
+            className="relative overflow-hidden min-h-screen bg-green-700 bg-cover bg-center bg-fixed"
+            style={{ backgroundImage: `url(${Bacground})` }}
+        >
+            <div className="relative z-10 bg-white rounded-lg shadow-xl p-6 sm:p-8 w-full sm:w-3/4 lg:w-1/2 xl:w-1/3 min-h-[400px] sm:min-h-[500px]">
                 <RegisterForm />
-                <RegisterForm onCancel={handleCancel} />
+            </div>
         </div>
     );
 };
