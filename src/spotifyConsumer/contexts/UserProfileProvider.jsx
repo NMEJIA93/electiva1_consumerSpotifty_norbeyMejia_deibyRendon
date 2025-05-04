e32096997108d14760472449adc8313f60c7c220
NMEJIA93 <norbeymejiacortes@gmail.com>
2025-05-04 16:29:53 -0500

    UPDATE UserProfileProvider

--- src/spotifyConsumer/contexts/UserProfileProvider.jsx
+++ src/spotifyConsumer/contexts/UserProfileProvider.jsx
@@ -1,10 +1,23 @@
 import { useReducer, useState } from 'react';
-import {userProfileReducer} from '../reducers/spotifyReducer'
-import {UserProfileContext} from  '../contexts/UserProfileContext'
+import { userProfileReducer } from '../reducers/spotifyReducer'
+import { UserProfileContext } from '../contexts/UserProfileContext'
+import { useProfile } from '../hooks/useProfile'
 
 // Estado inicial del perfil del usuario
 const initialProfileState = {
     profile: null,
     playlists: [],
     error: null,
-  };
+};
+
+export const UserProfileProvider = ({ children }) => {
+    const [profileState, dispatch] = useReducer(userProfileReducer, initialProfileState);
+    const {getProfile, getSpotifyUser} = useProfile(dispatch);
+
+    return (
+        <UserProfileContext.Provider
+            value={{ profileState, getProfile, getSpotifyUser}}>
+            {children}
+        </UserProfileContext.Provider>
+    );
+}
